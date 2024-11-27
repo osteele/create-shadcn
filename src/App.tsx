44bf347f67ecf2aa6fc35e7160a4beda15db2685
Oliver Steele <steele@osteele.com>
2024-11-27 12:29:23 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="max-w-xl mx-auto h-screen bg-slate-100 rounded-lg p-4">
+      <h1 className="text-4xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
